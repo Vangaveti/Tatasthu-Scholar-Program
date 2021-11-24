2b363e5c73b14fceaa706659c8bbc4ca5f7eca6d
Vangaveti <63050550+Vangaveti@users.noreply.github.com>
2021-11-24 10:05:14 +0530

    Create StringReverse.java

---
+++ StringReverse.java
@@ -0,0 +1,11 @@
+  import java.util.*;
+class reverse 
+{
+  public static void main(String args[])
+  {
+    Scanner sc = new Scanner(System.in);
+    String s = sc.nextLine();
+    StringBuilder sb = new StringBuilder(s);
+    System.out.print(sb.reverse());
+  }
+}
